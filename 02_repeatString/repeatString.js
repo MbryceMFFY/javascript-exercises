3d3ec3cb14ae2cbde8081c80b6dea660213dd923
MbryceMFFY <markb@mffy.com>
2023-04-29 18:07:03 +0100

    added repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let finalWord = "";
+    for (i = 0; i < times; i++) {
+        finalWord += word;
+    }
+    return finalWord;
 };
 
 // Do not edit below this line
