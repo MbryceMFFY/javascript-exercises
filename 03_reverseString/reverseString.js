cff85fdf73ec24d2fc677bf65c09ff70378ec850
MbryceMFFY <markb@mffy.com>
2023-04-29 18:51:12 +0100

    Added reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let currentWord = word;
+    let newWord = "";
+    let wordLength = word.length;
+    for (i = wordLength; i >= 0; i--) {
+        newWord += currentWord.charAt(i);
+    }
+    return newWord;
 };
 
 // Do not edit below this line
