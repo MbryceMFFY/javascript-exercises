a7af378f5db0ca1877f905349eef07b5171ad47d
MbryceMFFY <markb@mffy.com>
2023-04-30 12:35:28 +0100

    sumAll done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, item) {
+    const newArray = [];
+    array.forEach(myFunction);
+    function myFunction(value) {
+        if (value != item) {
+            newArray.push(value);
+        };
+        console.log(newArray);
+        return newArray;
+        
+    }
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    for (i = num1; i < (num2 + 1); i++) {
+        total = total + i;
+    }
+    return total;
 };
 
 // Do not edit below this line
