7498bccdc26a5b012b4e133d61c48f06c2dda038
MbryceMFFY <markb@mffy.com>
2023-04-30 12:52:12 +0100

    tempconver done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let num = (far - 32) * (5/9);
+  let result = Math.round(num*10)/10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let num = (cel * 2) + 30;
+  let result = Math.round(num*10)/10;
+  return result;
 };
 
 // Do not edit below this line
